7deb13e6299c194411b4ed83971722c3ffb6ad54
Aventure20 <aventure.dev.code@gmail.com>
2021-06-28 10:19:48 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# FrenchPhineNumberGenerator
+ 
---
+++ generator.py
@@ -0,0 +1,14 @@
+import random
+num0 = "0"
+num1 ="67"
+num2 = num3 = num4 = num5 = num6 = num7 = num8 = num9 = "1234567890"
+numr1 = random.choice(num1)
+numr2 = random.choice(num2)
+numr3 = random.choice(num3)
+numr4 = random.choice(num4)
+numr5 = random.choice(num5)
+numr6 = random.choice(num6)
+numr7 = random.choice(num7)
+numr8 = random.choice(num8)
+numr9 = random.choice(num9)
+print(num0 + numr1 + numr2 + numr3 + numr4 + numr5 + numr6 + numr7 + numr8 + numr9)
