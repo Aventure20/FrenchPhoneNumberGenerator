efbf042cc8d12bbff976a3f8d0cd4f22189bc547
Aventure20 <aventure.dev.code@gmail.com>
2021-07-23 14:07:23 +0200

    Debug num var

--- generator.py
+++ generator.py
@@ -1,14 +1,14 @@
 import random
 num0 = "0"
 num1 ="67"
-numO = "1234567890"
+num = "1234567890"
 numr1 = random.choice(num1)
-numr2 = random.choice(numO)
-numr3 = random.choice(numO)
-numr4 = random.choice(num0)
-numr5 = random.choice(numO)
-numr6 = random.choice(numO)
-numr7 = random.choice(numO)
-numr8 = random.choice(numO)
-numr9 = random.choice(numO)
+numr2 = random.choice(num)
+numr3 = random.choice(num)
+numr4 = random.choice(num)
+numr5 = random.choice(num)
+numr6 = random.choice(num)
+numr7 = random.choice(num)
+numr8 = random.choice(num)
+numr9 = random.choice(num)
 print(num0 + numr1 + numr2 + numr3 + numr4 + numr5 + numr6 + numr7 + numr8 + numr9)
